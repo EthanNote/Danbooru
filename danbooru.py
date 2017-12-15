9e802556b07911bcdf1230540820ce0b68da5dd5
EthanNote <hanyishen32@foxmail.com>
2017-12-15 16:48:19 +0800

    Update danbooru.py

--- danbooru.py
+++ danbooru.py
@@ -7,14 +7,14 @@ import threading
 import shutil
 
 def webread(url, readtimeout=10):
-	"""read page"""
+    """read page"""
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'),
                                     ('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')]
     return opener.open(url,None, readtimeout).read()
 
 def trywebread(url, timeout, retrytimes=3):
-	"""read page, retry if failed"""
+    """read page, retry if failed"""
     data=None
     retrycount=0
     while retrycount<=retrytimes:
